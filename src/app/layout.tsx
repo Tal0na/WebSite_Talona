ab2ebb526f9bb432d5e50bc620ef9527d82f10b9
Tales Costa <talonacontato@gmail.com>
2023-11-25 01:24:12 -0300

    Add files via upload

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import Head from 'next/head';
+
 import { ReactNode } from 'react';
 import { Poppins, Roboto } from 'next/font/google';
 import './globals.css';
@@ -20,25 +21,25 @@ export const metadata = {
   title: "Talona - Design e Desenvolvimento Web",
   description: "Explore nosso portfólio para experiências web excepcionais. Contate-nos para colaborações inovadoras!",
   keywords: "design web, desenvolvimento web, portfólio, inovação",
-  author: "Seu Nome",
-  ogImage: "URL_DA_IMAGEM_PARA_REDES_SOCIAIS",
+  author: "Tales Costa",
+  ogImage: '/svgs/Union.svg',
 };
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
+    <>
     <html lang="pt-BR" className={`${customFontPoppins.variable} ${customFontRoboto.variable}`}>
       <Head>
-        {/* Metadados */}
         <title>{metadata.title}</title>
         <meta name="description" content={metadata.description} />
         <meta name="keywords" content={metadata.keywords} />
         <meta name="author" content={metadata.author} />
         <meta property="og:image" content={metadata.ogImage} />
-        {/* Outras tags do cabeçalho que você pode precisar adicionar */}
       </Head>
       <body>
         {children}
       </body>
     </html>
+    </>
   );
 }
