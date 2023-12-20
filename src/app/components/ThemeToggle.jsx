84272a51b7ba7d06344be123f34819cea4b3f49c
Tales Costa <talonacontato@gmail.com>
2023-12-20 17:54:32 -0300

    Add files via upload

--- src/app/components/ThemeToggle.jsx
+++ src/app/components/ThemeToggle.jsx
@@ -1,40 +1,29 @@
+'use clinet'
 import { useTheme } from "next-themes";
 import { useEffect, useState } from "react";
-import { FaSun, FaMoon } from 'react-icons/fa';
 
 const ThemeButton = () => {
   const { resolvedTheme, setTheme } = useTheme();
+  
   const [mounted, setMounted] = useState(false);
-
   useEffect(() => setMounted(true), []);
 
-  const handleClick = () => {
-    setTheme(resolvedTheme === "dark" ? "light" : "dark");
-  };
+ if (!mounted){
+  return null
+ }
 
   return (
     <button
       aria-label="Toggle Dark Mode"
       type="button"
-      className={`dark:text-white text-black px-4 py-2 rounded flex items-center`}
-      onClick={typeof window !== "undefined" ? handleClick : undefined}
+      className={`dark:text-white text-black px-4 py-2 rounded`}
+      onClick={()=> setTheme(resolvedTheme === 'dark'?  'light' :'dark')}
     >
-      {mounted && (
+
         <>
-          {resolvedTheme === "dark" ? (
-            <>
-            
-              <FaMoon style={{ marginRight: '1rem' }} />
-             
-            </>
-          ) : (
-            <>
-              
-              <FaSun style={{ marginRight: '0.5rem' }} />
-            </>
-          )}
+          {resolvedTheme === "dark" ? "Light" : "Dark"}
         </>
-      )}
+  
     </button>
   );
 };
