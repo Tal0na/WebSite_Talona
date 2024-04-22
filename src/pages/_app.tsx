81fcf8a3570e318333d869c3fabf9d08b5b43ff7
Tales Costa <talonacontato@gmail.com>
2024-04-22 20:23:07 -0300

    b

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,48 +1,10 @@
 import "../app/globals.css";
-import { useEffect, ComponentType } from 'react';
+import {ComponentType} from 'react';
 import { ThemeProvider } from "next-themes";
 import Head from 'next/head';
 import Footer from '../app/components/Footer/Footer';
 import { Analytics } from "@vercel/analytics/react"
-import ReactGA from 'react-ga';
-import { useRouter } from 'next/router';
 
-// Inicialização do Google Analytics
-ReactGA.initialize('G-44JJVVPVM6');
-
-// Função para rastrear mudanças de página
-function trackPageviews() {
-  const router = useRouter();
-
-  useEffect(() => {
-    // Adicionando script do Google Tag Manager
-    const script1 = document.createElement('script');
-    script1.src = 'https://www.googletagmanager.com/gtag/js?id=G-44JJVVPVM6';
-    script1.async = true;
-    document.head.appendChild(script1);
-
-    // Adicionando script de configuração do Google Analytics
-    const script2 = document.createElement('script');
-    script2.innerHTML = `
-      window.dataLayer = window.dataLayer || [];
-      function gtag(){dataLayer.push(arguments);}
-      gtag('js', new Date());
-      gtag('config', 'G-44JJVVPVM6');
-    `;
-    document.head.appendChild(script2);
-
-    const handleRouteChange = (url: string) => {
-      // Rastreamento de página
-      ReactGA.pageview(url);
-    };
-
-    router.events.on('routeChangeComplete', handleRouteChange);
-
-    return () => {
-      router.events.off('routeChangeComplete', handleRouteChange);
-    };
-  }, [router.events]);
-}
 
 interface MyAppProps {
   Component: ComponentType<any>;
@@ -50,9 +12,6 @@ interface MyAppProps {
 }
 
 function MyApp({ Component, pageProps }: MyAppProps) {
-  // Rastreamento de página
-  trackPageviews();
-
   return (
     <ThemeProvider
       attribute="class"
