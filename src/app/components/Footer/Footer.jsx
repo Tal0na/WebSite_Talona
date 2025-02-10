2ca11ddefd150dd7dcba0d4cad59d7d7d9b179f9
Tales Costa <talonacontato@gmail.com>
2025-02-10 15:59:23 -0300

    Update Footer.jsx

--- src/app/components/Footer/Footer.jsx
+++ src/app/components/Footer/Footer.jsx
@@ -1,16 +1,14 @@
-'use client'
-import React, { useState } from "react";
-import LogoComponent from "../logo/logotheme";
+"use client"
+import React, { useState } from "react"
+import LogoComponent from "../logo/logotheme"
 
 const Footer = () => {
-
-
   return (
     <>
       <footer className="dark:bg-[#121212] bg-[#f9f9f9] border-t border-gray-300 dark:border-zinc-800">
         <div className="container px-5 py-6 mx-auto flex items-center sm:flex-row flex-col">
           <a className="flex title-font font-medium items-center md:justify-start justify-center dark:text-white text-black">
-          <LogoComponent />
+            <LogoComponent />
           </a>
           <p className="text-sm dark:text-white text-black sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-zinc-300 sm:py-2 sm:mt-0 mt-4">
             &copy; 2023 - {new Date().getFullYear()} &middot; Talona Costa —
@@ -106,18 +104,24 @@ const Footer = () => {
               className="ml-3 mt-1 dark:text-white text-black"
             >
               <svg
-               xmlns="http://www.w3.org/2000/svg"
-               fill="currentColor"
-               stroke="currentColor"
-               strokeLinecap="round"
-               strokeLinejoin="round"
-               strokeWidth="0"
-               className="w-6 h-6" 
-               viewBox="0 0 24 24" 
+                xmlns="http://www.w3.org/2000/svg"
+                fill="currentColor"
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="0"
+                className="w-6 h-6"
+                viewBox="0 0 24 24"
               >
-                
                 <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z" />
               </svg>
+            </a>
+            <a
+              href="https://mastodon.social/@Talona_"
+              rel="me"
+              className=" dark:text-white text-black"
+            >
+              Mastodon
             </a>
           </span>
         </div>
