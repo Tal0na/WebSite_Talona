0800a27fd15f219f86dd0a20353e78d224a316be
Tales Costa <talonacontato@gmail.com>
2025-03-26 21:28:55 -0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import About from "./components/About/About"
 import Footer from "./components/Footer/Footer"
 
 export default async function Home() {
+
   return (
     <>
       <main className="dark:bg-[#121212] bg-[#f9f9f9] min-h-screen flex flex-col justify-between">
